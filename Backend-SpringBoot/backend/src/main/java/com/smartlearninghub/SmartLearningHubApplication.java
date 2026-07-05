97ad491e42db73ac10d1887fcfc84d450d2c77c2
RadhikaTalokar <talokarradhika@gmail.com>
2026-07-05 09:00:00 +0530

    SmartlaerninghubApplication added

---
+++ Backend-SpringBoot/backend/src/main/java/com/smartlearninghub/SmartLearningHubApplication.java
@@ -0,0 +1,15 @@
+package com.smartlearninghub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Entry point for the Smart Learning Hub backend application.
+ */
+@SpringBootApplication
+public class SmartLearningHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartLearningHubApplication.class, args);
+    }
+}
